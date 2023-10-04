845abcb4c558233e7a941d5777ae70af6d930d75
CyberN0mad <71142523+CyberN0mad@users.noreply.github.com>
2023-10-04 21:39:30 +0800

    Added: Message Dialog

--- src/com/ContactForm.java
+++ src/com/ContactForm.java
@@ -2,30 +2,36 @@ package com;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ContactForm extends JFrame {
 
+    JTextField name_field, email_field;
+    JRadioButton male, female;
+    JCheckBox check;
+
     public ContactForm(){
         super("Contact Form");
-        super.setBounds(200, 100, 250, 100);
+        super.setBounds(200, 100, 300, 230);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Container container = super.getContentPane();
-        container.setLayout(new GridLayout(3,2,2,2));
+        container.setLayout(new GridLayout(5,2,2,10));
 
         JLabel name = new JLabel("Write your name:");
-        JTextField name_field = new JTextField("", 1);
+        name_field = new JTextField("", 1);
         JLabel email = new JLabel("Write your email:");
-        JTextField email_field = new JTextField("@", 1);
+        email_field = new JTextField("@", 1);
 
         container.add(name);
         container.add(name_field);
         container.add(email);
         container.add(email_field);
 
-        JRadioButton male = new JRadioButton("Male");
-        JRadioButton female = new JRadioButton("Female");
-        JCheckBox check = new JCheckBox("Agree?", false);
+        male = new JRadioButton("Male");
+        female = new JRadioButton("Female");
+        check = new JCheckBox("Agree?", false);
         JButton send_button = new JButton("Send");
 
         male.setSelected(true);
@@ -39,6 +45,28 @@ public class ContactForm extends JFrame {
         container.add(check);
         container.add(send_button);
 
+        send_button.addActionListener(new ButtonEventManager());
+    }
 
+    class ButtonEventManager implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String name = name_field.getText();
+            String email = email_field.getText();
+
+            String isMale = "Male";
+            if(!male.isSelected())
+                isMale = "Female";
+
+            boolean checkBox = check.isSelected();
+
+            JOptionPane.showMessageDialog(null,
+                    "Your email: " + email +
+                    "\nYour male: " + isMale + "\nAre you sure? " + checkBox,
+                    "Hello," + name,
+                    JOptionPane.PLAIN_MESSAGE);
+
+        }
     }
 }
