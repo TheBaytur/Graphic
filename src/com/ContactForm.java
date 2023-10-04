9314ade80c2a3e3066a7c62d30042ea225f0d7ef
CyberN0mad <71142523+CyberN0mad@users.noreply.github.com>
2023-10-04 20:47:21 +0800

    Graphic-ContactForm

---
+++ src/com/ContactForm.java
@@ -0,0 +1,17 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ContactForm extends JFrame {
+
+    public ContactForm(){
+        super("Contact Form");
+        super.setBounds(200, 100, 250, 100);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container container = super.getContentPane();
+        container.setLayout(new GridLayout(3,2,2,2));
+
+    }
+}
---
+++ src/com/Main.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ContactForm form = new ContactForm();
+        form.setVisible(true);
+
+//        form.setVisible(false);
+
+    }
+}
